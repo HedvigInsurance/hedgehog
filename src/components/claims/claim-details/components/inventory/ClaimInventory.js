dfdcaccefd3569b2ddafb6d61b7651d2d20fdc5b
rallemannen <31433805+rallemannen@users.noreply.github.com>
2020-02-26 17:21:17 +0100

    Restructure queries & mutation, add category dropdown

--- src/components/claims/claim-details/components/inventory/ClaimInventory.js
+++ src/components/claims/claim-details/components/inventory/ClaimInventory.js
@@ -1,84 +1,178 @@
 import Pricing from 'features/pricing'
-import { GET_INVENTORY } from 'features/pricing/queries'
+import { GET_INVENTORY, GET_CATEGORIES } from 'features/pricing/queries'
+import { ADD_ITEM } from 'features/pricing/mutations'
 import * as React from 'react'
-import { Query } from 'react-apollo'
+import { Query, Mutation } from 'react-apollo'
 import { Paper } from '../../../../shared/Paper'
 import { InventoryList } from './InventoryList'
 import { TextField } from '@material-ui/core'
+import MenuItem from '@material-ui/core/MenuItem'
 
 export class ClaimInventory extends React.Component {
   state = {
     itemName: '',
     itemValue: '',
-    itemCategory: '',
+    itemCategory: 'Övrigt',
+    items: [],
   }
 
   handleChange = (event) => {
-    const { target: { name, value } } = event
+    const {
+      target: { name, value },
+    } = event
     this.setState(() => ({ [name]: value }))
+  }
+
+  formLooksGood = () => {
+    return (
+      /^[0-9]+$/.test(this.state.itemValue) &&
+      this.state.itemValue !== '' &&
+      this.state.itemCategory !== 'None' &&
+      this.state.itemName !== ''
+    )
   }
 
   render() {
     return (
-      <Query
-        query={GET_INVENTORY}
-        variables={{
-          claimId: this.props.claimId,
-        }}
+      <Mutation
+        mutation={ADD_ITEM}
+        refetchQueries={() => [
+          {
+            query: GET_INVENTORY,
+            variables: { claimId: this.props.claimId },
+          },
+        ]}
       >
-        {({ data, loading, error }) => {
-          let items = []
-
-          if (!loading) {
-            if (!error) {
-              if (typeof data !== 'undefined') {
-                if ('inventory' in data) {
-                  items = data.inventory
-                }
-              }
-            }
+        {(addItem, addItemMutation) => {
+          if (addItemMutation.error) {
+            return <React.Fragment />
           }
 
           return (
-            <Paper>
-              <div>
-                <h3>Inventory</h3>
-              </div>
-              <InventoryList items={items} claimId={this.props.claimId} />
-              <div style={{ display: 'inline-flex' }}>
-                <TextField
-                  id="item-name"
-                  style={{ width: '48%' }}
-                  color="secondary"
-                  name="itemName"
-                  placeholder="Item"
-                  value={this.state.itemName}
-                  onChange={this.handleChange}
-                  helperText={"Press Enter to add item"}
-                />
-                <TextField
-                  id="item-value"
-                  style={{ width: '18%', marginLeft: '2%'}}
-                  color="secondary"
-                  placeholder="Value"
-                  name="itemValue"
-                  value={this.state.itemValue}
-                  onChange={this.handleChange}
-                />
-                <TextField
-                  id="item-category"
-                  style={{ width: '25%', marginLeft: '2%'}}
-                  color="secondary"
-                  placeholder="Category"
-                  name="itemCategory"
-                  value={this.state.itemCategory}
-                  onChange={this.handleChange}
-                />
-              </div>
-            </Paper>
+            <Query query={GET_CATEGORIES}>
+              {({
+                data: dataCategories,
+                loading: loadingCategories,
+                error: errorCategories,
+              }) => {
+                return (
+                  <Query
+                    query={GET_INVENTORY}
+                    variables={{
+                      claimId: this.props.claimId,
+                    }}
+                  >
+                    {({
+                      data: dataInventory,
+                      loading: loadingInventory,
+                      error: errorInventory,
+                    }) => {
+                      if (!loadingInventory) {
+                        if (!errorInventory) {
+                          if (typeof dataInventory !== 'undefined') {
+                            if ('inventory' in dataInventory) {
+                              if (
+                                this.state.items !== dataInventory.inventory
+                              ) {
+                                this.setState({
+                                  items: dataInventory.inventory,
+                                })
+                              }
+                            }
+                          }
+                        }
+                      }
+
+                      return (
+                        <Paper>
+                          <div>
+                            <h3>Inventory</h3>
+                          </div>
+                          <InventoryList
+                            items={this.state.items}
+                            claimId={this.props.claimId}
+                          />
+
+                          <div style={{ display: 'inline-flex' }}>
+                            <TextField
+                              id="item-name"
+                              style={{ width: '48%' }}
+                              color="secondary"
+                              label="Item"
+                              name="itemName"
+                              value={this.state.itemName}
+                              onChange={this.handleChange}
+                              helperText={this.formLooksGood() ? 'Press Return to add item ↩' : ' '}
+                            />
+
+                            <TextField
+                              id="item-category"
+                              select
+                              color="secondary"
+                              label="Category"
+                              name="itemCategory"
+                              style={{ width: '25%', marginLeft: '2%' }}
+                              value={this.state.itemCategory}
+                              onChange={this.handleChange}
+                            >
+                              <MenuItem value="Övrigt">Övrigt</MenuItem>
+
+                              {loadingCategories || errorCategories ? (
+                                <MenuItem value="Övrigt">Övrigt</MenuItem>
+                              ) : (
+                                dataCategories.categories.map((category) => (
+                                  <MenuItem
+                                    key={category.id}
+                                    value={category.name}
+                                  >
+                                    {category.name}
+                                  </MenuItem>
+                                ))
+                              )}
+                            </TextField>
+
+                            <TextField
+                              id="item-value"
+                              style={{ width: '18%', marginLeft: '2%' }}
+                              color="secondary"
+                              label="Value"
+                              name="itemValue"
+                              value={this.state.itemValue}
+                              onChange={this.handleChange}
+                              onKeyPress={async (e) => {
+                                if (e.key === 'Enter') {
+                                  console.log('Adding item...')
+                                  await addItem({
+                                    variables: {
+                                      item: {
+                                        inventoryItemId: null,
+                                        claimId: this.props.claimId,
+                                        itemName: this.state.itemName,
+                                        categoryName: this.state.itemCategory,
+                                        categoryId: '-1',
+                                        value: this.state.itemValue,
+                                        source: 'Custom',
+                                        upperRange: null,
+                                        lowerRange: null,
+                                        itemId: null,
+                                        filters: [],
+                                      },
+                                    },
+                                  })
+                                }
+                              }}
+                            />
+                          </div>
+                        </Paper>
+                      )
+                    }}
+                  </Query>
+                )
+              }}
+            </Query>
           )
         }}
-      </Query>
+      </Mutation>
     )
   }
 }
