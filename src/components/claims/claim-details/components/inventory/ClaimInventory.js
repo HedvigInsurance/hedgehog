c3435d9b5ec594638bf9d5badaedd6cc1b313089
rallemannen <31433805+rallemannen@users.noreply.github.com>
2020-02-28 11:50:03 +0100

    Add "add" button, apply grid for layout

--- src/components/claims/claim-details/components/inventory/ClaimInventory.js
+++ src/components/claims/claim-details/components/inventory/ClaimInventory.js
@@ -5,10 +5,9 @@ import * as React from 'react'
 import { Query, Mutation } from 'react-apollo'
 import { Paper } from '../../../../shared/Paper'
 import { InventoryList } from './InventoryList'
-import { TextField } from '@material-ui/core'
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline'
+import { TextField, Button, Grid } from '@material-ui/core'
 import MenuItem from '@material-ui/core/MenuItem'
-
+import AddIcon from '@material-ui/icons/Add'
 
 export class ClaimInventory extends React.Component {
   state = {
@@ -115,91 +114,113 @@ export class ClaimInventory extends React.Component {
                             <h3>Inventory</h3>
                           </div>
 
-                          {
-                            this.state.items.length !== 0 ?
+                          {this.state.items.length !== 0 ? (
                             <InventoryList
-                            items={this.state.items}
-                            claimId={this.props.claimId}
+                              items={this.state.items}
+                              claimId={this.props.claimId}
                             />
-                            : <></>
-                          }
+                          ) : (
+                            <></>
+                          )}
 
-                          <div style={{ display: 'flex'}}>
-                            <TextField
-                              id="item-name"
-                              style={{ width: '46%' }}
-                              color="secondary"
-                              placeholder="New item"
-                              name="itemName"
-                              value={this.state.itemName}
-                              onChange={this.handleChange}
-                              onKeyPress={async (e) => {
-                                if (e.key === 'Enter' && this.formLooksGood()) {
-                                  e.target.blur()
-                                  await addItem({
-                                    variables: {
-                                      item: this.getCurrentItem(),
-                                    },
-                                  })
+                          <Grid container spacing={24}>
+                            <Grid item xs={5}>
+                              <TextField
+                                fullWidth
+                                id="item-name"
+                                color="secondary"
+                                placeholder="New item"
+                                name="itemName"
+                                value={this.state.itemName}
+                                onChange={this.handleChange}
+                                onKeyPress={async (e) => {
+                                  if (
+                                    e.key === 'Enter' &&
+                                    this.formLooksGood()
+                                  ) {
+                                    e.target.blur()
+                                    await addItem({
+                                      variables: {
+                                        item: this.getCurrentItem(),
+                                      },
+                                    })
 
-                                  this.clearNewItem()
+                                    this.clearNewItem()
+                                  }
+                                }}
+                                helperText={
+                                  this.formLooksGood()
+                                    ? 'Press Return to add item ↩'
+                                    : ' '
                                 }
-                              }}
-                              helperText={
-                                this.formLooksGood()
-                                  ? 'Press Return to add item ↩'
-                                  : ' '
-                              }
-                            />
-
-                            <TextField
-                              id="item-category"
-                              select
-                              color="secondary"
-                              name="itemCategory"
-                              style={{ width: '29%', marginLeft: '2%' }}
-                              value={this.state.itemCategory}
-                              onChange={this.handleChange}
-                            >
-                              <MenuItem value="Övrigt">Övrigt</MenuItem>
-
-                              {loadingCategories || errorCategories ? (
+                              />
+                            </Grid>
+                            <Grid item xs={4}>
+                              <TextField
+                                fullWidth
+                                id="item-category"
+                                name="itemCategory"
+                                select
+                                value={this.state.itemCategory}
+                                onChange={this.handleChange}
+                              >
                                 <MenuItem value="Övrigt">Övrigt</MenuItem>
-                              ) : (
-                                dataCategories.categories.map((category) => (
-                                  <MenuItem
-                                    key={category.id}
-                                    value={category.name}
-                                  >
-                                    {category.name}
-                                  </MenuItem>
-                                ))
-                              )}
-                            </TextField>
 
-                            <TextField
-                              id="item-value"
-                              style={{ width: '21%', marginLeft: '2%' }}
-                              color="secondary"
-                              name="itemValue"
-                              placeholder="Value"
-                              align="right"
-                              value={this.state.itemValue}
-                              onChange={this.handleChange}
-                              onKeyPress={async (e) => {
-                                if (e.key === 'Enter' && this.formLooksGood()) {
-                                  e.target.blur()
-                                  await addItem({
-                                    variables: {
-                                      item: this.getCurrentItem(),
-                                    },
-                                  })
+                                {loadingCategories || errorCategories ? (
+                                  <MenuItem value="Övrigt">Övrigt</MenuItem>
+                                ) : (
+                                  dataCategories.categories.map((category) => (
+                                    <MenuItem
+                                      key={category.id}
+                                      value={category.name}
+                                    >
+                                      {category.name}
+                                    </MenuItem>
+                                  ))
+                                )}
+                              </TextField>
+                            </Grid>
+                            <Grid item xs={3}>
+                              <TextField
+                                fullWidth
+                                id="item-value"
+                                color="secondary"
+                                name="itemValue"
+                                placeholder="Value"
+                                align="right"
+                                value={this.state.itemValue}
+                                onChange={this.handleChange}
+                                onKeyPress={async (e) => {
+                                  if (
+                                    e.key === 'Enter' &&
+                                    this.formLooksGood()
+                                  ) {
+                                    e.target.blur()
+                                    await addItem({
+                                      variables: {
+                                        item: this.getCurrentItem(),
+                                      },
+                                    })
 
-                                  this.clearNewItem()
-                                }
-                              }}
-                            />
-                          </div>
+                                    this.clearNewItem()
+                                  }
+                                }}
+                              />
+                            </Grid>
+                          </Grid>
+                          <Grid container alignItems="flex-start" justify="flex-end" direction="row" spacing={24}>
+                            <Grid item xs={3}>
+                              <Button
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                display="inline"
+                                disabled={!this.formLooksGood()}
+                              >
+                                Add item
+                              </Button>
+                            </Grid>
+                          </Grid>
                         </Paper>
                       )
                     }}
