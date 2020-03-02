e13b5dff698a41102e973d80ef95b87a10cf0385
rallemannen <31433805+rallemannen@users.noreply.github.com>
2020-03-02 11:08:42 +0100

    Remove inline styling, implement withStyles

--- src/components/claims/claim-details/components/ClaimInventory.js
+++ src/components/claims/claim-details/components/ClaimInventory.js
@@ -9,6 +9,7 @@ import {
   TableCell,
   TableBody,
   IconButton,
+  withStyles,
 } from '@material-ui/core'
 import MenuItem from '@material-ui/core/MenuItem'
 import DeleteForeverIcon from '@material-ui/icons/Delete'
@@ -19,36 +20,59 @@ import * as React from 'react'
 import { Query, Mutation } from 'react-apollo'
 import { Paper } from '../../../shared/Paper'
 
+const InventoryTable = withStyles({
+  root: {
+    marginBottom: '25px',
+  },
+})(Table)
+
+const InventoryTableCell = withStyles({
+  root: {
+    paddingLeft: '0px'
+  },
+})(TableCell)
+
+const DeleteIcon = withStyles({
+    root: {
+      margin: '0px',
+      padding: '4px',
+      color: '#666',
+    },
+})(DeleteForeverIcon)
+
+
 class InventoryList extends React.Component {
   render() {
     return (
-      <Table size="small" style={{ marginBottom: '25px' }}>
+      <InventoryTable size="small">
         <colgroup>
           <col style={{ width: '45%' }} />
           <col style={{ width: '24%' }} />
           <col style={{ width: '20%' }} />
           <col style={{ width: '2%' }} />
         </colgroup>
+
         <TableHead>
           <TableRow>
-            <TableCell style={{ paddingLeft: '0px' }}>Item</TableCell>
-            <TableCell style={{ paddingLeft: '0px' }}>Category</TableCell>
-            <TableCell align="right" style={{ paddingLeft: '0px' }}>
+            <InventoryTableCell>Item</InventoryTableCell>
+            <InventoryTableCell>Category</InventoryTableCell>
+            <InventoryTableCell align="right">
               Value
-            </TableCell>
-            <TableCell style={{ paddingLeft: '0px' }}></TableCell>
+            </InventoryTableCell>
+            <InventoryTableCell/>
           </TableRow>
         </TableHead>
+
         <TableBody>
           {this.props.items.map((row) => (
             <TableRow key={row.inventoryItemId}>
-              <TableCell style={{ paddingLeft: '0px' }}>
+              <InventoryTableCell>
                 {row.itemName}
-              </TableCell>
-              <TableCell style={{ paddingLeft: '0px' }}>
+              </InventoryTableCell>
+              <InventoryTableCell>
                 {row.categoryName}
-              </TableCell>
-              <TableCell style={{ paddingLeft: '0px' }} align="right">
+              </InventoryTableCell>
+              <InventoryTableCell align="right">
                 {formatMoney(
                   'sv-SE',
                   0,
@@ -56,8 +80,8 @@ class InventoryList extends React.Component {
                   amount: row.value,
                   currency: 'SEK',
                 })}
-              </TableCell>
-              <TableCell style={{ paddingLeft: '0px' }}>
+              </InventoryTableCell>
+              <InventoryTableCell>
                 <Mutation
                   mutation={REMOVE_ITEM}
                   refetchQueries={() => {
@@ -73,7 +97,7 @@ class InventoryList extends React.Component {
                     return (
                       <IconButton
                         aria-label="Delete item"
-                        onClick={(e) => {
+                        onClick={() => {
                           removeItem({
                             variables: {
                               inventoryItemId: row.inventoryItemId,
@@ -81,22 +105,16 @@ class InventoryList extends React.Component {
                           })
                         }}
                       >
-                        <DeleteForeverIcon
-                          style={{
-                            margin: '0px',
-                            padding: '4px',
-                            color: '#666',
-                          }}
-                        />
+                        <DeleteIcon/>
                       </IconButton>
                     )
                   }}
                 </Mutation>
-              </TableCell>
+              </InventoryTableCell>
             </TableRow>
           ))}
         </TableBody>
-      </Table>
+      </InventoryTable>
     )
   }
 }
